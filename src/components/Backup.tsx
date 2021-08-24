391dcff052ce9091023af100f7e335e6a3436bd4
chazzox <caylott@outlook.com>
2021-08-24 02:19:58 +0100

    new display format

--- src/components/Backup.tsx
+++ src/components/Backup.tsx
@@ -1,38 +1,64 @@
 import type { Component } from 'solid-js';
-import { onMount, createSignal } from 'solid-js';
+import { onMount, createSignal, createEffect } from 'solid-js';
 import { styled } from 'solid-styled-components';
 
 const Playlist = styled('div')`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	background-color: ${(props) => props.theme.green};
-	margin: 45px;
-	padding: 8px;
-	border-radius: 8px;
-	box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;
-	padding-bottom: 25px;
 	& > img {
 		height: 150px;
 	}
 `;
 
+const Song = styled('div')`
+	display: flex;
+	background-color: ${(props) => props.theme.green};
+	padding: 15px;
+	margin: 15px;
+	border-radius: 8px;
+	& > img {
+		height: 50px;
+		margin-left: 15px;
+	}
+`;
+
 const Backup: Component<{ token: string }> = ({ token }) => {
-	const [playlists, setPlaylists] = createSignal([]);
+	const [discoverWeekly, setDiscoverWeekly] = createSignal({ name: '', images: [], id: '' });
+	const [songList, setSongList] = createSignal([]);
+
 	onMount(() => {
-		const requestURL = new URL('https://api.spotify.com/v1/me/playlists');
-		fetch(requestURL.toString(), { method: 'GET', headers: { Authorization: 'Bearer ' + token } })
+		fetch('https://api.spotify.com/v1/me/playlists', {
+			method: 'GET',
+			headers: { Authorization: 'Bearer ' + token }
+		})
 			.then((req) => req.json())
-			.then((playlists) => setPlaylists(playlists.items));
+			.then((playlists) => setDiscoverWeekly(playlists.items.filter((temp) => temp.name === 'Discover Weekly')[0]));
 	});
+
+	createEffect(() => {
+		discoverWeekly().id !== '' &&
+			fetch(`https://api.spotify.com/v1/playlists/${discoverWeekly().id}/tracks`, {
+				method: 'GET',
+				headers: { Authorization: 'Bearer ' + token }
+			})
+				.then((req) => req.json())
+				.then((songListTemp) => setSongList(songListTemp.items));
+	});
+
 	return (
 		<div>
-			{playlists().map((playlist) => (
-				<Playlist>
-					<h3>{playlist.name}</h3>
-					{playlist.images.length > 0 && <img src={playlist.images[0].url} alt="" />}
-				</Playlist>
-			))}
+			<Playlist>
+				<h1>{discoverWeekly().name}</h1>
+				{discoverWeekly().images.length > 0 && <img src={discoverWeekly().images[0].url} alt="" />}
+				<p>No. of Songs: {songList().length}</p>
+				{songList().map((song) => (
+					<Song>
+						<p>{song.track.name}</p>
+						<img src={song.track.album.images[0].url} alt="" />
+					</Song>
+				))}
+			</Playlist>
 		</div>
 	);
 };
