2c1ea511515f085ec4c5f048f3bed453c6ee832f
chazzox <caylott@outlook.com>
2021-08-24 01:04:16 +0100

    fixed issues with redirect not going to correct url

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -20,8 +20,8 @@ function generateAuthURL() {
 	const authURL = new URL('https://accounts.spotify.com/authorize');
 	authURL.searchParams.append('show_dialog', 'true');
 	authURL.searchParams.append('response_type', 'token');
-	authURL.searchParams.append('client_id', 'b487c0ac409c4b4aba8703323c9e8305');
-	authURL.searchParams.append('redirect_uri', 'http://localhost:3000/replay/');
+	authURL.searchParams.append('client_id', import.meta.env.VITE_API_TOKEN);
+	authURL.searchParams.append('redirect_uri', import.meta.env.VITE_REDIRECT_URI);
 	authURL.searchParams.append(
 		'scope',
 		['playlist-modify-private', 'playlist-read-private', 'playlist-modify-public'].join(' ')
@@ -32,6 +32,7 @@ function generateAuthURL() {
 const Login: Component = () => {
 	return (
 		<MainBox>
+			<h1 style="margin-top:1px;">REPLAY</h1>
 			<h3>Sign in to spotify and backup this weeks discover weekly!</h3>
 			<LoginLink href={generateAuthURL()}>Login to spotify</LoginLink>
 		</MainBox>
