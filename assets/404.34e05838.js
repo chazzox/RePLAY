071ff15447e3735789cdfd378618bb6966c4dd79
chazzox <chazzox@users.noreply.github.com>
2022-02-05 01:55:34 +0000

    deploy: 2b4b6892fea819a51cc143fe21641bd779d0de7e

---
+++ assets/404.34e05838.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill.b7f2da20.js";import{r as o}from"./vendor.68fb79f2.js";o((()=>"wrong page bozo"),document.getElementById("root"));
--- assets/404.fdf529e1.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill.b7f2da20.js";import{r as o}from"./vendor.9fb884c4.js";o((()=>"wrong page bozo"),document.getElementById("root"));
