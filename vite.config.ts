0a685e1e1ab89f830cdb3507dcd10d609003444b
chazzox <caylott@outlook.com>
2022-06-13 23:49:12 +0100

    fix(wip): hunting down issue with env vars not loading into prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import { resolve } from 'path';
 
+function envChecker(): Plugin {
+	return {
+		name: 'env-checker',
+		buildStart() {
+			console.log('logging stuff');
+			console.log(process.env);
+		}
+	};
+}
+
 export default defineConfig({
-	plugins: [solidPlugin()],
+	plugins: [solidPlugin(), envChecker()],
 	build: {
 		rollupOptions: {
 			input: { root: resolve(__dirname, 'index.html'), '404': resolve(__dirname, '404.html') }
